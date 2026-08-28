74e6d87c5789d1d511d6c67a850b8756e3d80a4a
Gamal <gamal.overspeed@gmail.com>
2026-08-28 17:16:00 +0400

    feat: add Dexie database schema

---
+++ src/db/db.ts
@@ -0,0 +1,15 @@
+import Dexie, { Table } from "dexie";
+import type { FlightEntry } from "../types/flightEntry";
+
+export class LogbookDatabase extends Dexie {
+  flightEntries!: Table<FlightEntry, number>;
+
+  constructor(name = "LogbookDatabase") {
+    super(name);
+    this.version(1).stores({
+      flightEntries: "++id, date, aircraftType, role"
+    });
+  }
+}
+
+export const db = new LogbookDatabase();
